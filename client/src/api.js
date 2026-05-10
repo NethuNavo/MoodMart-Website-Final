d25fd296dc7bd707d9924724404ccfdac552986b
NethuNavo <nethunavo24@gmail.com>
2026-05-10 12:22:44 +0530

    Remove duplicate client folder

--- client/src/api.js
+++
@@ -1,7 +0,0 @@
-const API = import.meta.env.VITE_API_URL || 'http://localhost:5000'
-
-export async function getProducts() {
-  const res = await fetch(`${API}/api/products`)
-  if (!res.ok) throw new Error('Failed to fetch')
-  return res.json()
-}
