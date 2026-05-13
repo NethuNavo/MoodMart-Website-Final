3ec9667b6ecc74a3d95977f8cd28f9642f8507db
Nethmini Jayamani <nethunavo24@gmail.com>
2026-05-14 04:29:00 +0530

    Delete update-env.ps1

--- update-env.ps1
+++
@@ -1,4 +0,0 @@
-$value = 'mongodb+srv://moodmart:pass123m@cluster0.qablwfr.mongodb.net/MoodMart?retryWrites=true&w=majority'
-$confirm = 'y'
-$input = $value + "`n" + $confirm + "`n"
-$input | npx --yes vercel env update MONGO_URI production
